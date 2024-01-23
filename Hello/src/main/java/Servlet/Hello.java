940c1d601d3ca6521db308e5b136ae2c878db7b8
baophucish <phuc.hoangbao304@gmail.com>
2024-01-23 13:42:44 +0700

    reup

---
+++ Hello/src/main/java/Servlet/Hello.java
@@ -0,0 +1,31 @@
+package Servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/hello")
+public class Hello extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Hello() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter pw = response.getWriter();
+		pw.write("<h1>Nguyen Hoang Bao Phuc</h1>");
+	}
+
+}
