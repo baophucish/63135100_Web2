c8260a9a9d82299b9cf24744714f4b0328c51d90
baophucish <phuc.hoangbao304@gmail.com>
2024-03-26 15:14:24 +0700

    up

---
+++ nguyenhoangbaophuc_qlbanhang/src/main/java/thigk_ntu63135100/nguyenhoangbaophuc_qlbanhang/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package thigk_ntu63135100.nguyenhoangbaophuc_qlbanhang.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("/")
+    public String home() {
+        return "index";
+    }
+
+}
