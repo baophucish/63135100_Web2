53fd76643188dfd011b090121e6835a6f361f8ee
baophucish <phuc.hoangbao304@gmail.com>
2024-05-14 11:12:55 +0700

    update DB

---
+++ to_chuc_giao_dien/src/main/java/com/example/to_chuc_giao_dien/ToChucGiaoDienApplication.java
@@ -0,0 +1,13 @@
+package com.example.to_chuc_giao_dien;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToChucGiaoDienApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToChucGiaoDienApplication.class, args);
+	}
+
+}
---
+++ to_chuc_giao_dien/src/test/java/com/example/to_chuc_giao_dien/ToChucGiaoDienApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.to_chuc_giao_dien;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToChucGiaoDienApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
